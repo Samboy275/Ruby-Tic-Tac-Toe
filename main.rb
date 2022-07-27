b72107507fb6e603cec8704c79313aa9f0398dfb
samual moahmed <sam-wal@windowslive.com>
2022-07-27 17:43:26 +0200

    Initial Commit

---
+++ main.rb
@@ -0,0 +1,6 @@
+# 1 - make a class for board to draw the board in console
+  # 1 - class should contain a way of storing board elements
+  # 2 - class should check for winning conditions
+  # 3 - board should have states to know the state of the game
+  # 4 - should make a function to draw the board with the current elements
+  # 5 - must register player movements
